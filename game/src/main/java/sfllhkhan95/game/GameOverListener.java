7c2ae9af59e42e4ae182e9826bef9051ca9bdd6a
Muhammad Saifullah Khan <sfllhkhan95@live.com>
2017-03-17 08:48:38 +0500

    Added game core files.

---
+++ game/src/main/java/sfllhkhan95/game/GameOverListener.java
@@ -0,0 +1,11 @@
+package sfllhkhan95.game;
+
+/**
+ * Created by saifkhichi96 on 16/03/2017.
+ */
+
+public interface GameOverListener {
+
+    void onGameOver();
+
+}
---
+++ game/src/test/java/sfllhkhan95/game/sound/GameSoundTest.java
@@ -0,0 +1,8 @@
+package sfllhkhan95.game.sound;
+
+/**
+ * Created by Muhammad Saifullah K on 07/03/2017.
+ */
+public class GameSoundTest {
+
+}
